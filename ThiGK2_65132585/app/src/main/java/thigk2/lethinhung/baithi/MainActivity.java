7ca537a3aa2b400b446d34e79b3381bc0283a8b0
lethinhungcr113-collab <lethinhungcr113@gmail.com>
2026-05-04 08:54:55 +0700

    qlct

--- ThiGK2_65132585/app/src/main/java/thigk2/lethinhung/baithi/MainActivity.java
+++ ThiGK2_65132585/app/src/main/java/thigk2/lethinhung/baithi/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
