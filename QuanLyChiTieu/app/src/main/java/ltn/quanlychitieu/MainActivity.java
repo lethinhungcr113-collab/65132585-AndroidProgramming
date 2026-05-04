6ba793c2e25493abcb2b021c5e718c05f542c7e1
lethinhungcr113-collab <lethinhungcr113@gmail.com>
2026-05-04 09:17:22 +0700

    add

--- QuanLyChiTieu/app/src/main/java/ltn/quanlychitieu/MainActivity.java
+++ QuanLyChiTieu/app/src/main/java/ltn/quanlychitieu/MainActivity.java
@@ -1,6 +1,8 @@
 package ltn.quanlychitieu;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +33,23 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");//key
 
         myRef.setValue("Hello, LTTBDD");//value
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                // xu ly dl
+                Toast.makeText(getBaseContext(), "Value is" + value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(getBaseContext(), "Failed to read value" , Toast.LENGTH_SHORT).show();
+                 }
+        });
     }
 }
